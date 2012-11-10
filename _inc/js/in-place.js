7f776d7cd76aefdc0cd72a0836d631e9cc2e76d2
Mike <mike@trenvo.com>
2012-11-10 14:10:17 +0200

    Fixed jQuery issue for older versions

--- _inc/js/in-place.js
+++ _inc/js/in-place.js
@@ -1,6 +1,6 @@
 jQuery(document).ready ( function ($) {
     // Block the following of links
-    $('a').on('click', function(e) {
+    $('a').bind('click', function(e) {
         e.preventDefault();
         e.stopPropagation();
     });
