03750e0533c2ce22e47cea6ef9b3f4f2870619d4
mike <mike@TrenvOS>
2012-11-14 08:25:21 +0200

    Lead by example - pageparentdiv now uses the transport api the right way

--- lib/meta-box-transports.php
+++ lib/meta-box-transports.php
@@ -2,23 +2,19 @@
 global $live_editor, $post;
 $post_type = $post->post_type;
 
-
 if ( post_type_supports($post_type, 'page-attributes') )
     $live_editor->add_metabox_transport( 'pageparentdiv', 'refresh' );
 
 function live_editor_pageparentdiv_transport() {
-    if ( isset ( $_GET[ 'page_template' ] ) && !empty ( $_GET[ 'page_template' ] ) ) {
-        add_filter('get_post_metadata', 'live_editor_pageparentdiv_load_template', 10, 4);
-    }
+    add_filter('get_post_metadata', 'live_editor_pageparentdiv_load_template', 10, 4);
 }
-add_action ('live_editor_iframe_init', 'live_editor_pageparentdiv_transport');
-//add_action ('setup_theme', 'live_editor_pageparentdiv_transport');
+add_action ( 'live_editor_transports-page_template', 'live_editor_pageparentdiv_transport' );
 
 function live_editor_pageparentdiv_load_template( $meta, $post_id, $meta_key, $single ) {
-    global $post;
+    global $post, $live_editor;
 
     if ( $post_id == $post->ID && $meta_key == '_wp_page_template' && $single = true )
-        return $_GET['page_template'];
+        return $live_editor->transport_params['page_template'];
 }
 
 
