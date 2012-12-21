5bed8adafe55a85fbfb1e9ebdc18140e97ae05b4
Mike <mike@trenvo.com>
2012-12-21 12:17:50 +0200

    First pass at converting to the live admin library

--- _inc/js/in-place.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready ( function ($) {
-    // Block the following of links
-    $('a').bind('click', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-    });
-});
